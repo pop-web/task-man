c8564498e21ba46f7071c2cd78834b45a87a8396
koga yusuke <popweb782@gmail.com>
2021-03-12 01:10:25 +0900

    update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -11,13 +11,13 @@ const {
 } = process.env;
 
 const firebaseConfig = {
-  apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: REACT_APP_FIREBASE_STRAGE_BUCKET,
-  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
-  appId: REACT_APP_FIREBASE_APP_ID,
+  apiKey:"AIzaSyBfpFzDoBPps3Q11bDAMhq9kZ0WURwCfJo",
+  authDomain: "task-man-web.firebaseapp.com",
+  projectId: "task-man-web",
+  storageBucket: "task-man-web.appspot.com",
+  messagingSenderId: "190716425692",
+  measurementId: "G-D0KYJ46DZV",
+  appId:"1:190716425692:web:280c6d0597c4a0f4a00b19",
 };
 firebase.initializeApp(firebaseConfig);
 
