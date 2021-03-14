6ef945c25ae7b19b3103725bd9bf3cef1b080263
koga yusuke <popweb782@gmail.com>
2021-03-14 15:38:25 +0900

    update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,14 +2,25 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STRAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_MEASUREMENT_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
+
 const firebaseConfig = {
-  apiKey:"AIzaSyBfpFzDoBPps3Q11bDAMhq9kZ0WURwCfJo",
-  authDomain: "task-man-web.firebaseapp.com",
-  projectId: "task-man-web",
-  storageBucket: "task-man-web.appspot.com",
-  messagingSenderId: "190716425692",
-  measurementId: "G-D0KYJ46DZV",
-  appId:"1:190716425692:web:280c6d0597c4a0f4a00b19",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STRAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 firebase.initializeApp(firebaseConfig);
 
