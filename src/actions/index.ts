9f39909d8360aa45a42a54cb6045fa34b3fb025d
koga yusuke <popweb782@gmail.com>
2021-03-14 15:49:44 +0900

    改行挿入

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,9 @@
 export const MODAL_OPEN = "MODAL_OPEN";
 export const MODAL_CLOSE = "MODAL_CLOSE";
+
 export const READ_TASKS = "READ_TASKS";
+
 export const EDIT_TASK = "EDIT_TASK";
+
 export const DONE_EDIT_TASK = "DONE_EDIT_TASK";
 export const LOGIN = "LOGIN";
